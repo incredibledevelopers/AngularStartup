8a8526d2dd6e1b940d92bb337481c5776bc79416
Amit Bhalerao <amitbaramatimca@gmail.com>
2019-10-19 14:24:05 +0530

    server.js

--- server.js
+++ server.js
@@ -4,13 +4,8 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('angularherokutestapp' + '/dist/<name-of-app>'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join('angularherokutestapp'+'/dist/<name-of-app>/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// All other routes should redirect to the index.html
+  app.route('/*')
+    .get(function(req, res) {
+      res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
+    });
