1292fa967c7a99082be99fac90ce264d458a4716
Amit Bhalerao <amitbaramatimca@gmail.com>
2019-10-19 14:17:33 +0530

    dirname

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-//Install express server    
+//Install express server
 const express = require('express');
-const path = require('path');   
-const app = express();   
+const path = require('path');
 
-// Serve only the static files form the dist directory    
+const app = express();
 
+// Serve only the static files form the dist directory
+app.use(express.static('angularherokutestapp' + '/dist/<name-of-app>'));
 
-app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')) ) ;
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join('angularherokutestapp'+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
