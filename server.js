34c2a00b43bca6f395c2379422edb630f9ee232d
Amit Bhalerao <amitbaramatimca@gmail.com>
2019-10-19 14:39:04 +0530

    ss

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
+// Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
